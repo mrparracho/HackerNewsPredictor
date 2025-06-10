6f7a5461067495ef7e7a7eda50386e7988b7b4f0
Ben Williams <ben.williams.20@ucl.ac.uk>
2025-06-10 17:39:01 +0100

    test commit

---
+++ scripts/build_dict.py
@@ -0,0 +1,9 @@
+from EDA.data_processor import TextProcessor
+
+
+# [(,),(,)...]
+
+# raw_text = 'this is a sentence yeah'
+# context = ['this', 'is', 'sentence' ,'yeah']
+# target = 'a'
+# [(context, target), (context, target)]
